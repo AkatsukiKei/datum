38e5d437212319e16954c4f6180f3d2514555153
AkatsukiKei <490423467@qq.com>
2019-07-15 14:17:06 +0800

    craw->crawl

--- python/baike_spider/spider_main.py
+++ python/baike_spider/spider_main.py
@@ -13,13 +13,13 @@ class SpiderMain(object):
         self.parser = html_parser.HtmlParser()
         self.outputer = html_outputer.HtmlOutputer()
         
-    def craw(self,root_url):
+    def crawl(self,root_url):
         count = 1
         self.urls.add_new_url(root_url)
         while self.urls.has_new_url():
             try:
                 new_url = self.urls.get_new_url()
-                print 'claw %d : %s' % (count, new_url)
+                print 'crawl %d : %s' % (count, new_url)
                 html_cont = self.downloader.download(new_url)
                 new_urls, new_data = self.parser.parser(new_url,html_cont)
                 self.urls.add_new_urls(new_urls)
@@ -30,7 +30,7 @@ class SpiderMain(object):
 
                 count = count+1
             except Exception, e:
-                print 'craw failed'
+                print 'crawler failed'
                 print 'str(Exception):\t', str(Exception)
                 print 'str(e):\t\t', str(e)
                 print 'repr(e):\t', repr(e)
@@ -43,4 +43,4 @@ class SpiderMain(object):
 if __name__ == "__main__":
     root_url = "https://baike.baidu.com/item/Python/407313"
     obj_spider = SpiderMain()
-    obj_spider.craw(root_url)
+    obj_spider.crawl(root_url)
